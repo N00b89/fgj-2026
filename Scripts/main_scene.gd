6d368b370226c3b41cbe0f995d91e33dc303a83f
N00b89 <168958557+N00b89@users.noreply.github.com>
2026-01-31 15:01:45 +0200

    latest version

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -1,6 +1,1 @@
 extends CanvasLayer
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("Pause"):
-		get_tree().paused = !get_tree().paused
-		$"Game Menu".visible = !$"Game Menu".visible
