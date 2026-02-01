8be252225da082c39973efe5af43f1b6315ab1e4
Arda Karaköz <ardakarakoz02@gmail.com>
2026-02-01 13:54:03 +0200

    SFXS

--- Scripts/AnimController.gd
+++ Scripts/AnimController.gd
@@ -4,11 +4,22 @@ class_name AnimationController
 var camera : Camera2D;
 func _ready():
 	camera = get_viewport().get_camera_2d()
-
-func _entrance():
+var randomVA :Array[String] = [
+		"va1","va2","va3","va8","va9","va10",
+		"va12","va13","va14","va15","va16","va17","va18","va19","va20",
+		"va21","va22"
+	]
+var leaveVA :Array[String] = [
+		"va4","va5", "va6","va7","va8", "va11"
+	]
+func _entrance(): 
 	play("entrance")
+	await get_tree().create_timer(.6).timeout
+	SoundManager.play_random_sfx(randomVA)
 func _exit():
 	play("exit")
+	await get_tree().create_timer(.3).timeout
+	SoundManager.play_random_sfx(leaveVA)
 	
 func _playStep():
 	camera.apply_shake(1.5)
--- Scripts/SoundManager.gd
+++ Scripts/SoundManager.gd
@@ -5,6 +5,30 @@ extends Node
 	"step2": {"stream": preload("res://sounds/fsfx-03.wav"), "volume": -5.0},
 	"step3": {"stream": preload("res://sounds/fsfx-04.wav"), "volume": -8.0},
 	"flip": {"stream": preload("res://sounds/fsfx-03.wav"), "volume": 12.0},
+	"va2": {"stream": preload("res://sounds/flash_va-02.wav"), "volume": -10.0},
+	"va3": {"stream": preload("res://sounds/flash_va-03.wav"), "volume": -10.0},
+	"va1": {"stream": preload("res://sounds/flash_va-01.wav"), "volume": -10.0},
+	"va4": {"stream": preload("res://sounds/flash_va-04.wav"), "volume": -10.0},
+	"va5": {"stream": preload("res://sounds/flash_va-05.wav"), "volume": -10.0},
+	"va6": {"stream": preload("res://sounds/flash_va-06.wav"), "volume": -10.0},
+	"va7": {"stream": preload("res://sounds/flash_va-07.wav"), "volume": -10.0},
+	"va8": {"stream": preload("res://sounds/flash_va-08.wav"), "volume": -10.0},
+	"va9": {"stream": preload("res://sounds/flash_va-09.wav"), "volume": -10.0},
+	"va10": {"stream": preload("res://sounds/flash_va-10.wav"), "volume": -10.0},
+	"va11": {"stream": preload("res://sounds/flash_va-11.wav"), "volume": -10.0},
+	"va12": {"stream": preload("res://sounds/flash_va-12.wav"), "volume": -10.0},
+	"va13": {"stream": preload("res://sounds/flash_va-13.wav"), "volume": -10.0},
+	"va14": {"stream": preload("res://sounds/flash_va-14.wav"), "volume": -10.0},
+	"va15": {"stream": preload("res://sounds/flash_va-15.wav"), "volume": -10.0},
+	"va16": {"stream": preload("res://sounds/flash_va-16.wav"), "volume": -10.0},
+	"va17": {"stream": preload("res://sounds/flash_va-17.wav"), "volume": -10.0},
+	"va18": {"stream": preload("res://sounds/flash_va-18.wav"), "volume": -10.0},
+	"va19": {"stream": preload("res://sounds/flash_va-19.wav"), "volume": -10.0},
+	"va20": {"stream": preload("res://sounds/flash_va-20.wav"), "volume": -10.0},
+	"va21": {"stream": preload("res://sounds/flash_va-21.wav"), "volume": -10.0},
+	"va22": {"stream": preload("res://sounds/flash_va-22.wav"), "volume": -10.0},
+
+
 	"classical1": {"stream": preload("res://sounds/classical_flash.wav"), "volume": -7.0}
 }
 func _ready():
